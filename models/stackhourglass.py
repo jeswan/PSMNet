4134ee8f6bd35976f4d677ff29e0ac514604ca9b
Jesse Swanson <js11133@nyu.edu>
2020-09-25 18:23:57 -0700

    error fixes

--- models/stackhourglass.py
+++ models/stackhourglass.py
@@ -5,7 +5,7 @@ import torch.utils.data
 from torch.autograd import Variable
 import torch.nn.functional as F
 import math
-from submodule import *
+from .submodule import *
 
 class hourglass(nn.Module):
     def __init__(self, inplanes):
@@ -29,7 +29,7 @@ class hourglass(nn.Module):
                                    nn.BatchNorm3d(inplanes)) #+x
 
     def forward(self, x ,presqu, postsqu):
-        
+
         out  = self.conv1(x) #in:1/4 out:1/8
         pre  = self.conv2(out) #in:1/8 out:1/8
         if postsqu is not None:
@@ -43,7 +43,7 @@ class hourglass(nn.Module):
         if presqu is not None:
            post = F.relu(self.conv5(out)+presqu, inplace=True) #in:1/16 out:1/8
         else:
-           post = F.relu(self.conv5(out)+pre, inplace=True) 
+           post = F.relu(self.conv5(out)+pre, inplace=True)
 
         out  = self.conv6(post)  #in:1/8 out:1/4
 
@@ -63,7 +63,7 @@ class PSMNet(nn.Module):
 
         self.dres1 = nn.Sequential(convbn_3d(32, 32, 3, 1, 1),
                                    nn.ReLU(inplace=True),
-                                   convbn_3d(32, 32, 3, 1, 1)) 
+                                   convbn_3d(32, 32, 3, 1, 1))
 
         self.dres2 = hourglass(32)
 
@@ -121,13 +121,13 @@ class PSMNet(nn.Module):
         cost0 = self.dres0(cost)
         cost0 = self.dres1(cost0) + cost0
 
-        out1, pre1, post1 = self.dres2(cost0, None, None) 
+        out1, pre1, post1 = self.dres2(cost0, None, None)
         out1 = out1+cost0
 
-        out2, pre2, post2 = self.dres3(out1, pre1, post1) 
+        out2, pre2, post2 = self.dres3(out1, pre1, post1)
         out2 = out2+cost0
 
-        out3, pre3, post3 = self.dres4(out2, pre1, post2) 
+        out3, pre3, post3 = self.dres4(out2, pre1, post2)
         out3 = out3+cost0
 
         cost1 = self.classif1(out1)
@@ -135,24 +135,24 @@ class PSMNet(nn.Module):
         cost3 = self.classif3(out3) + cost2
 
         if self.training:
-		cost1 = F.upsample(cost1, [self.maxdisp,left.size()[2],left.size()[3]], mode='trilinear')
-		cost2 = F.upsample(cost2, [self.maxdisp,left.size()[2],left.size()[3]], mode='trilinear')
+            cost1 = F.upsample(cost1, [self.maxdisp,left.size()[2],left.size()[3]], mode='trilinear')
+            cost2 = F.upsample(cost2, [self.maxdisp,left.size()[2],left.size()[3]], mode='trilinear')
 
-		cost1 = torch.squeeze(cost1,1)
-		pred1 = F.softmax(cost1,dim=1)
-		pred1 = disparityregression(self.maxdisp)(pred1)
+            cost1 = torch.squeeze(cost1,1)
+            pred1 = F.softmax(cost1,dim=1)
+            pred1 = disparityregression(self.maxdisp)(pred1)
 
-		cost2 = torch.squeeze(cost2,1)
-		pred2 = F.softmax(cost2,dim=1)
-		pred2 = disparityregression(self.maxdisp)(pred2)
+            cost2 = torch.squeeze(cost2,1)
+            pred2 = F.softmax(cost2,dim=1)
+            pred2 = disparityregression(self.maxdisp)(pred2)
 
-        cost3 = F.upsample(cost3, [self.maxdisp,left.size()[2],left.size()[3]], mode='trilinear')
-        cost3 = torch.squeeze(cost3,1)
-        pred3 = F.softmax(cost3,dim=1)
-	#For your information: This formulation 'softmax(c)' learned "similarity" 
-	#while 'softmax(-c)' learned 'matching cost' as mentioned in the paper.
-	#However, 'c' or '-c' do not affect the performance because feature-based cost volume provided flexibility.
-        pred3 = disparityregression(self.maxdisp)(pred3)
+            cost3 = F.upsample(cost3, [self.maxdisp,left.size()[2],left.size()[3]], mode='trilinear')
+            cost3 = torch.squeeze(cost3,1)
+            pred3 = F.softmax(cost3,dim=1)
+            #For your information: This formulation 'softmax(c)' learned "similarity"
+            #while 'softmax(-c)' learned 'matching cost' as mentioned in the paper.
+            #However, 'c' or '-c' do not affect the performance because feature-based cost volume provided flexibility.
+            pred3 = disparityregression(self.maxdisp)(pred3)
 
         if self.training:
             return pred1, pred2, pred3
